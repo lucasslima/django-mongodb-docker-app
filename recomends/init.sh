c2b46f33641e529e968c8754823dda9d79fb30b4
Lucas Lima <lima.lucas.ma@gmail.com>
2018-03-29 18:25:41 -0300

    Removed unused command.

--- recomends/init.sh
+++ recomends/init.sh
@@ -1,4 +1,6 @@
 # Apply database migrations
+# É necessário fazer a migração antes de iniciar a aplicação,
+# portanto, criamos um entrepoint diferetente
 echo "Apply database migrations"
 python manage.py migrate
 
