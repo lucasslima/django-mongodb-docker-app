5c295a86751c9d8b4ac2c640f39a010c215057ed
Lucas Lima <lima.lucas.ma@gmail.com>
2018-03-29 00:41:52 -0300

    Remove unused comments

--- recomends/sugestions/urls.py
+++ recomends/sugestions/urls.py
@@ -5,12 +5,4 @@ from . import views
 app_name = 'sugestions'
 urlpatterns = [
     path('', views.contact, name='index'),
-    #path('contact/',views.contact, name='contact'),
-    #path('success/',views.successView, name='success'),
-    ## ex: /polls/5/
-    #path('<int:question_id>/', views.detail, name='detail'),
-    ## ex: /polls/5/results/
-    #path('<int:question_id>/results/', views.results, name='results'),
-    ## ex: /polls/5/vote/
-    #path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
